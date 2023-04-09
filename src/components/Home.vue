91fcec3d923518536b2c52b39340181a8a174008
vickypineli <v.pinero70@gmail.com>
2023-04-09 08:07:39 +0200

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+      <template #header>
+          <Header></Header>
+      </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from "./Layout.vue";
+import Header from "./Header.vue";
+export default {
+  components: {
+      Layout,
+      Header,
+  }
+};
+</script>
