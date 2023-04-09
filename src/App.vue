751468f8d2a60a69120d81e1da0d9c3dd97cf268
vickypineli <v.pinero70@gmail.com>
2023-04-09 09:12:07 +0200

    refactoriazar todo el proyecto configurar vue.config.js

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <Suspense>
     <template #default>
-      <Home/>
+      <Home />
     </template>
     <template #fallback>
-      <SplashScreen/>
+      <SplashScreen />
     </template>
   </Suspense>
 </template>
@@ -12,18 +12,20 @@
 <script>
 import SplashScreen from "./components/SplashScreen.vue";
 import { defineAsyncComponent } from "vue";
+
 export default {
   components: {
     SplashScreen,
-    Home: defineAsyncComponent(() => 
-      new Promise((resolve) => {
-        setTimeout(() => {
-          resolve(import("./components/Home.vue"))
-        }, 2500);
-      })
-    )
-}
-}
+    Home: defineAsyncComponent(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(import("./components/Home.vue"));
+          }, 2500);
+        })
+    ),
+  },
+};
 </script>
 
 <style>
@@ -34,6 +36,7 @@ body,
   margin: 0;
   font-family: Arial, Helvetica, sans-serif;
 }
+
 * {
   --brand-green: #04b500;
   --brand-blue: #0689b0;
